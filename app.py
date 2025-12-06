73dbb24f8965bdf947491217d3a2267bd21ec4a2
QuadDarv1ne <maksimqwe42@mail.ru>
2025-12-06 12:38:51 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -29,9 +29,12 @@ from utils.background_tasks import TaskQueue, TaskScheduler, CommonTasks, Schedu
 db = SQLAlchemy()
 login_manager = LoginManager()
 mail = Mail()
+from flask_limiter.util import get_remote_address
+
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
 )
 
 # Минификатор для оптимизации статических файлов
